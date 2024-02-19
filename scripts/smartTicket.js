d81107c84888d7abf5d1571cfcf38dd732d9fafa
ahmed-jobayer <jobayer320721@.com>
2024-02-19 17:58:02 +0600

    selected seat and avaiable seat function added

--- scripts/smartTicket.js
+++ scripts/smartTicket.js
@@ -1,19 +1,104 @@
 // Hide main show success page
 
 function next() {
-    hideElementById('primarySection')
-    showElementById('successSection')
+  hideElementById("primarySection");
+  showElementById("successSection");
 }
 
 // Hide success page show main page
 
 function continueBuying() {
-    hideElementById('successSection')
-    showElementById('primarySection')
+  hideElementById("successSection");
+  showElementById("primarySection");
 }
 
 // Add background color on click
 
-function addBackgroundColor(){
-    
+function addBackgroundColorA1() {
+  addBackgroundColorById("a1");
+  // update available seat
+  const currentSeat = getTextElementValueById("seatRemained");
+  const availableSeat = currentSeat - 1;
+  setTextElementValueById("seatRemained", availableSeat);
+  // update selected seat
+  const currentSelectedSeat = getTextElementValueById("seatSelected");
+  const selectedSeatCount = currentSelectedSeat + 1;
+  setTextElementValueById("seatSelected", selectedSeatCount);
+}
+function addBackgroundColorA2() {
+  addBackgroundColorById("a2");
+  // update available seat
+  const currentSeat = getTextElementValueById("seatRemained");
+  const availableSeat = currentSeat - 1;
+  setTextElementValueById("seatRemained", availableSeat);
+  // update selected seat
+  const currentSelectedSeat = getTextElementValueById("seatSelected");
+  const selectedSeatCount = currentSelectedSeat + 1;
+  setTextElementValueById("seatSelected", selectedSeatCount);
+}
+function addBackgroundColorA3() {
+  addBackgroundColorById("a3");
+  // update available seat
+  const currentSeat = getTextElementValueById("seatRemained");
+  const availableSeat = currentSeat - 1;
+  setTextElementValueById("seatRemained", availableSeat);
+  // update selected seat
+  const currentSelectedSeat = getTextElementValueById("seatSelected");
+  const selectedSeatCount = currentSelectedSeat + 1;
+  setTextElementValueById("seatSelected", selectedSeatCount);
+}
+function addBackgroundColorA4() {
+  addBackgroundColorById("a4");
+  // update available seat
+  const currentSeat = getTextElementValueById("seatRemained");
+  const availableSeat = currentSeat - 1;
+  setTextElementValueById("seatRemained", availableSeat);
+  // update selected seat
+  const currentSelectedSeat = getTextElementValueById("seatSelected");
+  const selectedSeatCount = currentSelectedSeat + 1;
+  setTextElementValueById("seatSelected", selectedSeatCount);
+}
+function addBackgroundColorB1() {
+  addBackgroundColorById("b1");
+  // update available seat
+  const currentSeat = getTextElementValueById("seatRemained");
+  const availableSeat = currentSeat - 1;
+  setTextElementValueById("seatRemained", availableSeat);
+  // update selected seat
+  const currentSelectedSeat = getTextElementValueById("seatSelected");
+  const selectedSeatCount = currentSelectedSeat + 1;
+  setTextElementValueById("seatSelected", selectedSeatCount);
+}
+function addBackgroundColorB2() {
+  addBackgroundColorById("b2");
+  // update available seat
+  const currentSeat = getTextElementValueById("seatRemained");
+  const availableSeat = currentSeat - 1;
+  setTextElementValueById("seatRemained", availableSeat);
+  // update selected seat
+  const currentSelectedSeat = getTextElementValueById("seatSelected");
+  const selectedSeatCount = currentSelectedSeat + 1;
+  setTextElementValueById("seatSelected", selectedSeatCount);
+}
+function addBackgroundColorB3() {
+  addBackgroundColorById("b3");
+  // update available seat
+  const currentSeat = getTextElementValueById("seatRemained");
+  const availableSeat = currentSeat - 1;
+  setTextElementValueById("seatRemained", availableSeat);
+  // update selected seat
+  const currentSelectedSeat = getTextElementValueById("seatSelected");
+  const selectedSeatCount = currentSelectedSeat + 1;
+  setTextElementValueById("seatSelected", selectedSeatCount);
+}
+function addBackgroundColorB4() {
+  addBackgroundColorById("b4");
+  // update available seat
+  const currentSeat = getTextElementValueById("seatRemained");
+  const availableSeat = currentSeat - 1;
+  setTextElementValueById("seatRemained", availableSeat);
+  // update selected seat
+  const currentSelectedSeat = getTextElementValueById("seatSelected");
+  const selectedSeatCount = currentSelectedSeat + 1;
+  setTextElementValueById("seatSelected", selectedSeatCount);
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,21 +1,34 @@
-
 // Add  hidden to classList
 
 function hideElementById(elementId) {
-    const element =document.getElementById(elementId)
-    element.classList.add('hidden');
+  const element = document.getElementById(elementId);
+  element.classList.add("hidden");
 }
 
 // Remove hiddden from class list
 
 function showElementById(elementId) {
-    const element =document.getElementById(elementId)
-    element.classList.remove('hidden');
+  const element = document.getElementById(elementId);
+  element.classList.remove("hidden");
 }
 
 // Add background color on click
 
-function addBackgroundColorById(elementId){
-    const element = document.getElementById(elementId)
-    element.classList.add('bgGreen')
+function addBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bgGreen");
+}
+
+// changing of seat count
+
+function getTextElementValueById(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValueText = element.innerText;
+  const value = parseInt(elementValueText);
+  return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
 }
